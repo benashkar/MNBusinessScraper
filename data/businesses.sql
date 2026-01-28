602014d16b8bb7d143d0a0500a9a4b809994555e
Ben Ashkar <cashkar@gmail.com>
2026-01-28 07:10:29 -0600

    Auto-save: 298335 records (2026-01-28 07:10)

--- data/businesses.sql
+++ data/businesses.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41d11435ea6e806a7e68e924da598bacebac7959f227153cb47b5b86db3bd2bb
-size 193127517
+oid sha256:77e4d3fd419bd25aa012dcfe9f777ac3d1c13db8e73048494f915c7ab7464156
+size 201200118
